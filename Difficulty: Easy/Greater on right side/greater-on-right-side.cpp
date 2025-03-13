cf88658a0a854406d0851d0959932d22f0db34d7
SATVIK909 <satvikmishra200015@gmail.com>
2025-03-13 15:51:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Greater on right side/greater-on-right-side.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution {
+  public:
+    /* Function to replace every element with the
+    next greatest element */
+    vector<int> nextGreatest(vector<int> arr) {
+        int mx=-1;
+        int n=arr.size();
+        for(int i=n-1;i>=0;i--){
+            int temp=mx;
+            if(arr[i]>mx){
+                temp=arr[i];
+            }
+            arr[i]=mx;
+            mx=temp;
+        }
+        return arr;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.nextGreatest(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
