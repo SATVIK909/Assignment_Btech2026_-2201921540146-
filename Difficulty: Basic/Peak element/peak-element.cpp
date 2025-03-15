33f25c892cceb22a26abe503785b1f93f35b5d97
SATVIK909 <satvikmishra200015@gmail.com>
2025-03-15 19:43:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Peak element/peak-element.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        int low = 0, high = arr.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // Check if mid is a peak element
+        if ((mid == 0 || arr[mid] > arr[mid - 1]) &&
+            (mid == arr.size() - 1 || arr[mid] > arr[mid + 1])) {
+            return mid; // Peak found
+        }
+
+        // If the right neighbor is greater, move right
+        if (mid < arr.size() - 1 && arr[mid] < arr[mid + 1]) {
+            low = mid + 1;
+        }
+        // Otherwise, move left
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1; // Should never reach here
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution ob;
+        int idx = ob.peakElement(a);
+        int n = a.size();
+        bool f = 0;
+        if (idx < 0 and idx >= n)
+            cout << "false" << endl;
+        else {
+            if (n == 1 and idx == 0)
+                f = 1;
+            else if (idx == 0 and a[0] > a[1])
+                f = 1;
+            else if (idx == n - 1 and a[n - 1] > a[n - 2])
+                f = 1;
+            else if (a[idx] > a[idx + 1] and a[idx] > a[idx - 1])
+                f = 1;
+            else
+                f = 0;
+            if (f)
+                cout << "true" << endl;
+            else
+                cout << "false" << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
